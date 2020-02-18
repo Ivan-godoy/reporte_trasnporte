289027d135188927745cf108dce29bd2a83a1ca8
Ivan-godoy <ivangodoy24@gmail.com>
2020-02-18 01:03:51 -0600

    Inicio del primer aporte Reporte de trasnporte en mal estado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+            app
+            v-model="drawer">
+      <!-- -->
+    </v-navigation-drawer>
+
+      <v-app-bar
+              app
+              color="white"
+              elevate-on-scroll
+              scroll-target="#scrolling-techniques-7"
+      >
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Title</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+<!--      <v-sheet
+              id="scrolling-techniques-7"
+              class="overflow-y-auto purple"
+              max-height="950"
+      >
+        <v-container app style="height: 1500px;">
+          <div>
+            <h1 class="text&#45;&#45;white">Hola Mundo</h1>
+          </div>
+        </v-container>
+      </v-sheet>-->
+    <v-content
+            id="scrolling-techniques-7"
+            class="overflow-auto"
+
+    >
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <h1>Hola Mundo</h1>
+        <!-- If using vue-router -->
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data(){
+    if (screen.width<1024){
+      return{
+        drawer: false
+      }
+    }else{
+      return{
+        drawer: true
+      }
+    }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  }
+};
+</script>
